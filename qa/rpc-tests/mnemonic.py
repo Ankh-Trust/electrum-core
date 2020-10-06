cfd32f6a179f827a5bc9efbc64ceac8bb3d7f017
hmlsanders <hml.sanders@outlook.com>
2020-10-07 01:26:15 +0200

    g

--- qa/rpc-tests/mnemonic.py
+++ qa/rpc-tests/mnemonic.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2019 Navcoin Core developers
+# Copyright (c) 2019 NavCoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
