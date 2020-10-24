fa7c81dac5b328950dca420a7807d4c4bf7a8d04
hmlsanders <hml.sanders@outlook.com>
2020-10-24 20:43:38 +0200

    Update electrum.cpp

--- src/qt/electrum.cpp
+++ src/qt/electrum.cpp
@@ -623,7 +623,7 @@ int main(int argc, char *argv[])
 
     // Load the app
     ElectrumApplication app(argc, argv);
-if QT_VERSION > 0x050100
+#if QT_VERSION > 0x050100
     // Generate high-dpi pixmaps
 #endif
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
@@ -644,7 +644,7 @@ if QT_VERSION > 0x050100
     //   Need to pass name here as CAmount is a typedef (see http://qt-project.org/doc/qt-5/qmetatype.html#qRegisterMetaType)
     //   IMPORTANT if it is no longer a typedef use the normal variant above
     qRegisterMetaType< CAmount >("CAmount");
-    qRegisterMetaType<std::function<void(void)> >("std::function<void(void)>");    
+    qRegisterMetaType<std::function<void(void)> >("std::function<void(void)>");
 
     /// 3. Application identification
     // must be set before OptionsModel is initialized or translations are loaded,
