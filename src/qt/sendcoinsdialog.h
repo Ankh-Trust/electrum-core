b3aac9856f8d2ee82d974860fb8c5e720552b3fd
hmlsanders <hml.sanders@outlook.com>
2020-10-31 00:22:35 +0100

    Revert "Update sendcoinsdialog.h"
    This reverts commit 1730fb78dd91664e37b4ed317b33b6f009a6e431.

--- src/qt/sendcoinsdialog.h
+++ src/qt/sendcoinsdialog.h
@@ -56,7 +56,6 @@ public Q_SLOTS:
     void updateTabsAndLabels();
     void setBalance(const CAmount& balance, const CAmount& unconfirmedBalance, const CAmount& stakingBalance, const CAmount& immatureBalance,
                     const CAmount& watchOnlyBalance, const CAmount& watchUnconfBalance, const CAmount& watchImmatureBalance, const CAmount& coldStakingBalance);
-    void useFullAmount();
 
 private:
     Ui::SendCoinsDialog *ui;
