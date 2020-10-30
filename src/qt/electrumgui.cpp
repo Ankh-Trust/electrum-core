f1939ddbc1fa7fb0df5fd5b81c7c1bbae53f9ca5
hmlsanders <hml.sanders@outlook.com>
2020-10-30 17:10:21 +0100

    Update electrumgui.cpp

--- src/qt/electrumgui.cpp
+++ src/qt/electrumgui.cpp
@@ -668,6 +668,7 @@ void ElectrumGUI::createToolBars()
         toolbar->addAction(sendCoinsAction);
         toolbar->addAction(receiveCoinsAction);
         toolbar->addAction(historyAction);
+        toolbar->addAction(daoAction);
 
         /** Create additional container for toolbar and walletFrame and make it the central widget.
             This is a workaround mostly for toolbar styling on Mac OS but should work fine for every other OSes too.
@@ -680,18 +681,6 @@ void ElectrumGUI::createToolBars()
         QWidget* containerWidget = new QWidget();
         containerWidget->setLayout(layout);
         setCentralWidget(containerWidget);
-
-/*
- *      topMenu5 = new QPushButton();
- *      walletFrame->menuLayout->addWidget(topMenu5);
- *      topMenu5->setFixedSize(215,94);
- *      topMenu5->setObjectName("topMenu5");
- *      connect(topMenu5, SIGNAL(clicked()), this, SLOT(gotoCommunityFundPage()));
- *      topMenu5->setStyleSheet(
- *                  "#topMenu5 { background: url(:/icons/menu_community_fund_ns) bottom center #eee; border: 0; }"
- *                  "#topMenu5:hover { background: url(:/icons/menu_community_fund_hover) bottom center #ddd; border: 0; }");
- */
-
     }
 #endif // ENABLE_WALLET
 
