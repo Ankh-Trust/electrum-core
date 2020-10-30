4221851bfa94e7d223b778db0a59ea4b044673fe
hmlsanders <hml.sanders@outlook.com>
2020-10-30 21:09:24 +0100

    Update electrumgui.cpp

--- src/qt/electrumgui.cpp
+++ src/qt/electrumgui.cpp
@@ -697,6 +697,7 @@ void ElectrumGUI::createToolBars()
         menuBtns[i]->setIcon(icon);
         menuBtns[i]->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
         menuBtns[i]->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred);
+        menuBtns[i]->setProperty("class", "main-menu-btn");        
 
         // Attach to the layout and assign click events
         walletFrame->menuLayout->addWidget(menuBtns[i]);
