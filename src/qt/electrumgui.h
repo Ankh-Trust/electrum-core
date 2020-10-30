715837609101da9fa2984ac1fb3857f034e0c7b8
hmlsanders <hml.sanders@outlook.com>
2020-10-30 14:19:56 +0100

    Update electrumgui.h

--- src/qt/electrumgui.h
+++ src/qt/electrumgui.h
@@ -261,6 +261,11 @@ public Q_SLOTS:
     void message(const QString &title, const QString &message, unsigned int style, bool *ret = NULL);
 
 #ifdef ENABLE_WALLET
+    /** Set the hd-enabled status as shown in the UI.
+    @param[in] status            current hd enabled status
+    @see WalletModel::EncryptionStatus
+    */
+    void setHDStatus(int hdEnabled);
     /** Set the encryption status as shown in the UI.
        @param[in] status            current encryption status
        @see WalletModel::EncryptionStatus
