9db3e0c374ee2122f8d36f6129fa8dbe5b979f43
Orestes-3113 <23432027+Orestes-3113@users.noreply.github.com>
2020-11-19 22:18:56 +0100

    Update transactionview.cpp

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -103,7 +103,7 @@ TransactionView::TransactionView(const PlatformStyle *platformStyle, QWidget *pa
     amountWidget = new QLineEdit(this);
     amountWidget->setPlaceholderText(tr("Min amount"));
     amountWidget->setAlignment(Qt::AlignRight);
-    amountWidget->setFixedWidth(AMOUNT_MINIMUM_COLUMN_WIDTH);
+    amountWidget->setFixedWidth(250);
     amountWidget->setValidator(new QDoubleValidator(0, 1e20, 8, this));
     hlayout->addWidget(amountWidget);
 
