964fa970c127a638585c340fdecd2a9b1271ba11
Orestes-3113 <23432027+Orestes-3113@users.noreply.github.com>
2020-11-22 00:02:26 +0100

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -291,8 +291,8 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        // vSeeds.push_back(CDNSSeedData("", ""));
-        // vSeeds.push_back(CDNSSeedData("", ""));
+        vSeeds.push_back(CDNSSeedData("ankh-trust.com", "dnsseed3.ankh-trust.com"));
+        vSeeds.push_back(CDNSSeedData("ankh-trust.com", "dnsseed4.ankh-trust.com"));
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
